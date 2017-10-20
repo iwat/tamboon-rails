266817839ae0bd50872964381fe6d3205108d276
iwat <iwatth@gmail.com>
2017-10-20 17:06:22 +0700

    Refactor #1, early return is better than deep cond

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -5,57 +5,61 @@ class WebsiteController < ApplicationController
 
   def donate
     charity = Charity.find_by(id: params[:charity])
-    if params[:omise_token].present?
-      unless params[:amount].blank? || params[:amount].to_i <= 20
-        unless !charity
-          if Rails.env.test?
-            charge = OpenStruct.new({
-              amount: params[:amount].to_i * 100,
-              paid: (params[:amount].to_i != 999),
-            })
-          else
-            charge = Omise::Charge.create({
-              amount: params[:amount].to_i * 100,
-              currency: "THB",
-              card: params[:omise_token],
-              description: "Donation to #{charity.name} [#{charity.id}]",
-            })
-          end
-          if charge.paid
-            charity.credit_amount(charge.amount)
-          end
-        else
-          @token = retrieve_token(params[:omise_token])
-          flash.now.alert = t(".failure")
-          render :index
-          return
-        end
-      else
-        @token = retrieve_token(params[:omise_token])
-        flash.now.alert = t(".failure")
-        render :index
-        return
-      end
-    else
+    if params[:omise_token].blank?
       @token = nil
       flash.now.alert = t(".failure")
       render :index
       return
     end
+
+    if params[:amount].blank? || params[:amount].to_i <= 20
+      @token = retrieve_token(params[:omise_token])
+      flash.now.alert = t(".failure")
+      render :index
+      return
+    end
+
+    unless charity
+      @token = retrieve_token(params[:omise_token])
+      flash.now.alert = t(".failure")
+      render :index
+      return
+    end
+
+    if Rails.env.test?
+      charge = OpenStruct.new({
+        amount: params[:amount].to_i * 100,
+        paid: (params[:amount].to_i != 999),
+      })
+    else
+      charge = Omise::Charge.create({
+        amount: params[:amount].to_i * 100,
+        currency: "THB",
+        card: params[:omise_token],
+        description: "Donation to #{charity.name} [#{charity.id}]",
+      })
+    end
+
+    if charge.paid
+      charity.credit_amount(charge.amount)
+    end
+
     if !charity
       @token = nil
       flash.now.alert = t(".failure")
       render :index
       return
     end
-    if charge.paid
-      flash.notice = t(".success")
-      redirect_to root_path
-    else
+
+    unless charge.paid
       @token = nil
       flash.now.alert = t(".failure")
       render :index
+      return
     end
+
+    flash.notice = t(".success")
+    redirect_to root_path
   end
 
   private
